ee2b4aaebc5c449c6cd2ec3855e953da6c72c54e
babeinlovexd <43302033+babeinlovexd@users.noreply.github.com>
2026-05-09 18:00:23 +0000

    feat: implement Version Broker logic in MAIN-CTRL
    Extended the MAIN-CTRL firmware to act as a version broker for an external
    PC-Flasher-Tool.
    - Enabled the ESPHome webserver on port 80.
    - Implemented cyclic polling of I2C registers 0xF0 (Major), 0xF1 (Minor),
      and 0xF2 (Patch) on slaves 0x20 (DSP), 0x21 (BT), and 0x22 (SUB).
    - Exposed the collected versions via a JSON-formatted ESPHome text_sensor.
    - Cleaned up generated __pycache__ from source control.

--- Firmware/Master/components/isb_orchestrator/__init__.py
+++ Firmware/Master/components/isb_orchestrator/__init__.py
@@ -1,18 +1,20 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import i2c
+from esphome.components import i2c, text_sensor
 from esphome.const import CONF_ID
 
-DEPENDENCIES = ['i2c']
+DEPENDENCIES = ['i2c', 'text_sensor']
 
 isb_orchestrator_ns = cg.esphome_ns.namespace('isb_orchestrator')
 ISBOrchestrator = isb_orchestrator_ns.class_('ISBOrchestrator', cg.Component)
 
 CONF_I2C_ID = 'i2c_id'
+CONF_VERSIONS_SENSOR = 'versions_sensor'
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(ISBOrchestrator),
     cv.Required(CONF_I2C_ID): cv.use_id(i2c.I2CBus),
+    cv.Optional(CONF_VERSIONS_SENSOR): cv.use_id(text_sensor.TextSensor),
 }).extend(cv.COMPONENT_SCHEMA)
 
 async def to_code(config):
@@ -20,3 +22,7 @@ async def to_code(config):
     await cg.register_component(var, config)
     i2c_bus = await cg.get_variable(config[CONF_I2C_ID])
     cg.add(var.set_i2c_bus(i2c_bus))
+
+    if CONF_VERSIONS_SENSOR in config:
+        sens = await cg.get_variable(config[CONF_VERSIONS_SENSOR])
+        cg.add(var.set_versions_sensor(sens))
